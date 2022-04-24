92d53ffa7f9b8e6cfd6fcb4bc2545a95fd2e0e9b
Deák Ruben <55796855+RubiMaistro@users.noreply.github.com>
2022-04-24 23:51:58 +0200

    Employee: add design for FoodList and FoodTypeList

--- Restaurant_EmployeeWebClient/Pages/Foods/FoodList.cs
+++ Restaurant_EmployeeWebClient/Pages/Foods/FoodList.cs
@@ -7,11 +7,18 @@ namespace Restaurant_EmployeeWebClient.Pages.Foods
     {
         [Inject]
         public HttpClient? HttpClient { get; set; }
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
         public IList<Food>? Foods { get; set; }
+
+        public string ImageServervicePath { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
+            ImageServervicePath = "https://localhost:7000/api/file";
+
             Foods = await HttpClient.GetFromJsonAsync<List<Food>>("food");
         }
+
     }
 }
