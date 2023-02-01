2560a0e54a07b71e0071d689721339df18662935
Ruben <55796855+RubiMaistro@users.noreply.github.com>
2023-02-01 18:12:39 +0200

    feat(Customer): add footer

---
+++ Restaurant_CustomerWebClient/Shared/Footer.cs
@@ -0,0 +1,36 @@
+﻿namespace Restaurant_CustomerWebClient.Shared
+{
+    public partial class Footer
+    {
+        /// <summary>
+        /// A megadott e-mail cím
+        /// </summary>
+        public string Email { get; set; } = "";
+
+        protected override void OnInitialized()
+        {
+            
+        }
+        /// <summary>
+        /// Feliratkozás a hírlevélre a megadott e-mail címen
+        /// </summary>
+        public void SubscribeToNewsletter()
+        {
+
+        }
+        /// <summary>
+        /// Működéssel kapcsolatos információk elfogadása
+        /// </summary>
+        public void Function1()
+        {
+
+        }
+        /// <summary>
+        /// Marketing célú ajánlatok efogadása
+        /// </summary>
+        public void Function2()
+        {
+
+        }
+    }
+}
